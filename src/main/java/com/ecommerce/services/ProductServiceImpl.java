33d77619e9002d9e51b9b4ef8c55a0092b93886d
Cazaks <calebezak@gmail.com>
2025-12-01 19:30:22 +0100

    Basic implementation for the create prodcut method to enable create product successfully test pass

--- src/main/java/com/ecommerce/services/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/services/ProductServiceImpl.java
@@ -1,4 +1,57 @@
 package com.ecommerce.services;
 
-public class ProductServiceImpl {
+
+import com.ecommerce.DTOs.request.productResponse.CreateProductRequest;
+import com.ecommerce.DTOs.response.productResponse.CreateProductResponse;
+import com.ecommerce.data.model.Product;
+import com.ecommerce.data.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Document(collection = "products")
+@Service
+@AllArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+
+    private ProductRepository productRepository;
+
+    @Override
+    public CreateProductResponse createProduct(CreateProductRequest createRequest){
+
+
+       if(createRequest.getProductName() == null || createRequest.getProductName().trim().isBlank()){
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product name cannot be empty");
+      }
+
+        Product productCreated = new Product();
+        productCreated.setProductName(createRequest.getProductName());
+        productCreated.setProductDescription(createRequest.getProductDescription());
+        productCreated.setProductPrice(createRequest.getProductPrice());
+        productCreated.setProductQuantity(createRequest.getProductQuantity());
+        productCreated.setProductCategory(createRequest.getProductCategory());
+        productCreated.setSellerId(createRequest.getSellerId());
+
+        productCreated = productRepository.save(productCreated);
+
+        CreateProductResponse createResponse = mapToProductResponse(productCreated);
+
+        return createResponse;
+
+    }
+
+    private static CreateProductResponse mapToProductResponse(Product productCreated) {
+        CreateProductResponse createResponse = new CreateProductResponse();
+        createResponse.setProductId(productCreated.getProductId());
+        createResponse.setProductName(productCreated.getProductName());
+        createResponse.setProductDescription(productCreated.getProductDescription());
+        createResponse.setProductPrice(productCreated.getProductPrice());
+        createResponse.setProductQuantity(productCreated.getProductQuantity());
+        createResponse.setProductCategory(productCreated.getProductCategory());
+        createResponse.setSellerId(productCreated.getSellerId());
+        return createResponse;
+    }
 }
