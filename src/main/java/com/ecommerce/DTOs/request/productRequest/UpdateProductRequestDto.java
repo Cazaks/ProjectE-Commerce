3d0ab87341835c053c11984f184bcf67c849f8e9
Cazaks <calebezak@gmail.com>
2025-12-08 14:40:45 +0100

    Field in request update dto

--- src/main/java/com/ecommerce/DTOs/request/productRequest/UpdateProductRequestDto.java
+++ src/main/java/com/ecommerce/DTOs/request/productRequest/UpdateProductRequestDto.java
@@ -1,5 +1,6 @@
 package com.ecommerce.DTOs.request.productRequest;
 
+import com.ecommerce.data.model.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,9 @@ public class UpdateProductRequestDto {
 
     private String productId;
     private String productName;
-    private Double productQuantity;
+    private int productQuantity;
     private String productDescription;
-    private String productPrice;
-    private String productCategory;
+    private Double productPrice;
+    private Category productCategory;
+//    private String message;
 }
