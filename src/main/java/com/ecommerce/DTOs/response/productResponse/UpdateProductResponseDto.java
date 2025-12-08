3c4d3b6e7b994ae459fc8cfcc96c0074cb199a0a
Cazaks <calebezak@gmail.com>
2025-12-08 14:43:48 +0100

    Response update dto fields

--- src/main/java/com/ecommerce/DTOs/response/productResponse/UpdateProductResponseDto.java
+++ src/main/java/com/ecommerce/DTOs/response/productResponse/UpdateProductResponseDto.java
@@ -1,4 +1,20 @@
 package com.ecommerce.DTOs.response.productResponse;
 
+import com.ecommerce.data.model.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateProductResponseDto {
+
+    private String productId;
+    private String productName;
+    private int productQuantity;
+    private String productDescription;
+    private Double productPrice;
+    private Category productCategory;
+    private String message;
 }
